bd1e6074229b4422424b29d1aea4baa95acb0d66
David Sandwell <dsandwell@ucsd.edu>
2020-09-09 16:07:25 -0700

    Update gmtsar.rb

--- gmtsar.rb
+++ gmtsar.rb
@@ -9,7 +9,7 @@ class Gmtsar < Formula
 #  url "https://topex.ucsd.edu/gmtsar/tar/GMTSAR_V5.8.tar"
 #  sha256 "25fe7f134734b14d7ffd620614d363e63d07038b9377e21828cb42999fd79b12"
   url "https://topex.ucsd.edu/gmtsar/tar/GMTSAR_V6.0.tar"
-  sha256 "beab80cda2279b8bd3bd42974d30bf68e73b3442010a642778f8a8960245db1a"
+  sha256 "4c14388ce77e064f08b2d40c06e73bb0bc3f3294077df993fc66f7c63325aa5e"
 
   option "without-gmt", "Does not install GMT; use this option only if you already installed GMT with all of the (previously optional) libraries (e.g., gdal, pcre, etc).  This is not recommended."
   
